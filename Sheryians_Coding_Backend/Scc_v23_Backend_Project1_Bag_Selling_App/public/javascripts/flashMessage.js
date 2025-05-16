9207a5b78e6fefd95688748052ea98a1ffdde22b
abhishekh1516techboy <abhishekhkumar1516@gmail.com>
2025-05-16 14:39:42 +0530

    Add Flash Notification Sound On Success & On Error

--- Sheryians_Coding_Backend/Scc_v23_Backend_Project1_Bag_Selling_App/public/javascripts/flashMessage.js
+++ Sheryians_Coding_Backend/Scc_v23_Backend_Project1_Bag_Selling_App/public/javascripts/flashMessage.js
@@ -4,18 +4,33 @@ setTimeout(() => {
   if (flash) {
     flash.classList.remove("translate-x-full", "opacity-0");
     flash.classList.add("translate-x-0", "opacity-100", "yoyo-bounce");
+
+    // Play sound based on flash background
+    if (flash.classList.contains("bg-green-600")) {
+      playSound("/audio/success.mp3");
+    } else if (flash.classList.contains("bg-red-600")) {
+      playSound("/audio/error.mp3");
+    }
   }
 }, 150);
 
 // Auto remove flash after 5 seconds
-setTimeout(() => {
-  removeFlash();
-}, 5000);
+setTimeout(removeFlash, 5000);
 
-// Function to remove flash
 function removeFlash() {
-  const flash = document.getElementById("flash-message-container");
+  const flash = document.getElementById("flash-message");
   if (flash) {
-    flash.remove();
+    flash.classList.remove("translate-x-0", "opacity-100");
+    flash.classList.add("translate-x-full", "opacity-0");
+    // Wait for animation to complete before removing
+    setTimeout(() => flash.remove(), 3000);
   }
 }
+
+// Function to play sound safely on success and on error
+const playSound = (src) => {
+  const audio = new Audio(src);
+  audio.play().catch((error) => {
+    console.error("Error playing sound:", error);
+  });
+};
