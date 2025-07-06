c98ca2859f90961e163df794ec3553ca84473527
abhishekh1516techboy <abhishekhkumar1516@gmail.com>
2025-07-06 23:26:56 +0530

    Handling Form Submition store shorted link in links.json file

--- ThapaTechnical_Backend/Tt_v31_URL_Shortner_App/public/script.js
+++ ThapaTechnical_Backend/Tt_v31_URL_Shortner_App/public/script.js
@@ -1,8 +1,25 @@
-document.getElementById("shortenForm").addEventListener("submit", (e) => {
+document.getElementById("shortenForm").addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const formData = new FormData(event.target);
   const url = formData.get("fullUrl");
   const shortUrl = formData.get("shortUrl");
-  
+
+  try {
+    const response = await fetch("/shorten", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ url, shortUrl }),
+    });
+
+    if (response.ok) {
+      alert("Form submitted Successfully!");
+      e.target.reset();
+    } else {
+      const errorMessage = await response.text();
+      alert(errorMessage);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 });
